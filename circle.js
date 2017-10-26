b4a11315c689b59b5771366d46885ffb5f57441d
jason13245 <jason13245@gmail.com>
2017-10-26 14:22:05 +0800

    Minor changes in circle js, major issue not yet fixed

--- circle.js
+++ circle.js
@@ -12,15 +12,14 @@ class DrawingCircle extends PaintFunction{
     }
     onDragging(coord,event){
         this.contextDraft.fillStyle = "#f44";
-        r=Math.sqrt(Math.pow((this.origX-coord[0]),2)+Math.pow((this.origY-coord[1]),2));
+        let r=Math.sqrt(Math.pow((this.origX-coord[0]),2)+Math.pow((this.origY-coord[1]),2));
         this.contextDraft.arc(this.origX,this.origY,r,0,2*Math.PI);
-
     }
 
     onMouseMove(){}
     onMouseUp(coord){
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
-        r=Math.sqrt(Math.pow((this.origX-coord[0]),2)+Math.pow((this.origY-coord[1]),2));
+        let r=Math.sqrt(Math.pow((this.origX-coord[0]),2)+Math.pow((this.origY-coord[1]),2));
         this.contextReal.arc(this.origX,this.origY,r,0,2*Math.PI);
         contextReal.stroke();
 
