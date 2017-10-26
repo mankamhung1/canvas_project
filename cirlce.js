54574800739465569ff558bffd54c52daa483a9e
jason13245 <jason13245@gmail.com>
2017-10-26 14:03:28 +0800

    Real draw circle js

---
+++ cirlce.js
@@ -0,0 +1,30 @@
+class DrawingCircle extends PaintFunction{
+    constructor(contextReal,contextDraft){
+        super();
+        this.contextReal = contextReal;
+        this.contextDraft = contextDraft;            
+    }
+    
+    onMouseDown(coord,event){
+        this.contextReal.fillStyle = "#f44";
+        this.origX = coord[0];
+        this.origY = coord[1];
+    }
+    onDragging(coord,event){
+        this.contextDraft.fillStyle = "#f44";
+        r=Math.sqrt(Math.pow((this.origX-coord[0]),2)+Math.pow((this.origY-coord[1]),2));
+        this.contextDraft.arc(this.origX,this.origY,r,0,2*Math.PI);
+        contextDraft.stroke();
+    }
+
+    onMouseMove(){}
+    onMouseUp(coord){
+        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+        r=Math.sqrt(Math.pow((this.origX-coord[0]),2)+Math.pow((this.origY-coord[1]),2));
+        this.contextReal.arc(this.origX,this.origY,r,0,2*Math.PI);
+        contextReal.stroke();
+
+    }
+    onMouseLeave(){}
+    onMouseEnter(){}
+}
