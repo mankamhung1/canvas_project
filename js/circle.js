fd609dd40f8d4694b8b22092b38816fa3ceb421d
Man KH <32627196+mankamhung1@users.noreply.github.com>
2017-10-26 16:54:18 +0800

    circle modifying

--- js/circle.js
+++ js/circle.js
@@ -10,17 +10,22 @@ class DrawingCircle extends PaintFunction{
         this.origY = coord[1];
     }
     onDragging(coord,event){
+      //  this.contextDraft.fillStyle = "#ffffff";
+        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+        this.contextDraft.beginPath();
         let r=Math.sqrt(Math.pow((this.origX-coord[0]),2)+Math.pow((this.origY-coord[1]),2));
         this.contextDraft.arc(this.origX,this.origY,r,0,2*Math.PI);
-        
+        this.contextDraft.stroke();
     }
 
     onMouseMove(){}
     onMouseUp(coord){
+    //    this.contextDraft.fillStyle = "#ffffff"
+        this.contextReal.strokeStyle = "#df4b26";
         let r=Math.sqrt(Math.pow((this.origX-coord[0]),2)+Math.pow((this.origY-coord[1]),2));
         this.contextReal.beginPath();
         this.contextReal.arc(this.origX,this.origY,r,0,2*Math.PI);
-        contextReal.stroke();
+        this.contextReal.stroke();
         this.contextReal.closePath();
     }
     onMouseLeave(){}
