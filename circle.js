eb229dcda620448c9577f861441bf20da25f3535
Man KH <32627196+mankamhung1@users.noreply.github.com>
2017-10-26 15:08:07 +0800

    solved circle dragging

--- circle.js
+++ circle.js
@@ -10,10 +10,13 @@ class DrawingCircle extends PaintFunction{
         this.origX = coord[0];
         this.origY = coord[1];
     }
+    
     onDragging(coord,event){
-        this.contextDraft.fillStyle = "#f44";
+        this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+        this.contextDraft.beginPath();
         let r=Math.sqrt(Math.pow((this.origX-coord[0]),2)+Math.pow((this.origY-coord[1]),2));
         this.contextDraft.arc(this.origX,this.origY,r,0,2*Math.PI);
+        this.contextDraft.stroke();
     }
 
     onMouseMove(){}
