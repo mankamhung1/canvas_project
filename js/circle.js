a2bcbf2eb3b5ddaf3465c17f5e026297c617b65d
jason13245 <jason13245@gmail.com>
2017-10-27 13:44:51 +0800

    Added change color, working on omg resize

--- js/circle.js
+++ js/circle.js
@@ -10,7 +10,7 @@ class DrawingCircle extends PaintFunction{
         this.origY = coord[1];
     }
     onDragging(coord,event){
-      //  this.contextDraft.fillStyle = "#ffffff";
+        this.contextDraft.strokeStyle = color;
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
         this.contextDraft.beginPath();
         let r=Math.sqrt(Math.pow((this.origX-coord[0]),2)+Math.pow((this.origY-coord[1]),2));
@@ -22,7 +22,7 @@ class DrawingCircle extends PaintFunction{
     onMouseUp(coord){
         this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
         this.contextReal.beginPath();
-        this.contextReal.strokeStyle = "#000000";
+        this.contextReal.strokeStyle = color;
         this.contextReal.lineWidth = 1;
         let r=Math.sqrt(Math.pow((this.origX-coord[0]),2)+Math.pow((this.origY-coord[1]),2));
         this.contextReal.arc(this.origX,this.origY,r,0,2*Math.PI);
