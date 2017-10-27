8a1723b048c743f636539b11583bc69348e00480
jason13245 <jason13245@gmail.com>
2017-10-27 14:37:47 +0800

    minor changes

--- js/resize.js
+++ js/resize.js
@@ -7,6 +7,7 @@ class Resize extends PaintFunction{
         this.image =image;
     }
     printImg() {
+        $(image_target).append('<img class="resize-image" src="Lenna.png" alt="image for resizing">')
         $(image).wrap('<div class="resize-container"></div>')
         .before('<span class="resize-handle resize-handle-nw"></span>')
         .before('<span class="resize-handle resize-handle-ne"></span>')
