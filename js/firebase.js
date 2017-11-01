fbb31a501cf94615d7de4b9a8ccca1946f9be7db
Man KH <32627196+mankamhung1@users.noreply.github.com>
2017-11-01 16:17:57 +0800

    FileName parameter is added for firebase importation/exportation

--- js/firebase.js
+++ js/firebase.js
@@ -1,69 +1,71 @@
 /////////// This part is for uploading to firebase:
-function uploadToFirebase() {
+function uploadToFirebase(name) {
   canvasReal.toBlob(function(blob) {
 
-  var file = blob ;
+          var file = blob;
 
-  // Create the file metadata
-  var metadata = {
-    contentType: 'image/jpeg'
-  };
+          // Create the file metadata
+          var metadata = {
+            contentType: 'image/png'
+          };
 
-  var storageRef = firebase.storage().ref();
-  var canvasRef = storageRef.child('canvas_images');
-  var fileName = "canvas.jpeg";
-  var canvasImagesRef = canvasRef.child(fileName);
+          var storageRef = firebase.storage().ref();
+          var canvasRef = storageRef.child('canvas_images');
+          var fileName = name;
+          var canvasImagesRef = canvasRef.child(fileName);
 
-  var uploadTask = canvasImagesRef.put(file, metadata);
+          var uploadTask = canvasImagesRef.put(file, metadata);
 
-  console.log(blob);
+          console.log(blob);
 
-  // Listen for state changes, errors, and completion of the upload.
-  uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-    function(snapshot) {
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log('Upload is paused');
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log('Upload is running');
-          break;
-      }
-    }, function(error) {
+          // Listen for state changes, errors, and completion of the upload.
+          uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+            function(snapshot) {
+              // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+              var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              console.log('Upload is ' + progress + '% done');
+              switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED: // or 'paused'
+                  console.log('Upload is paused');
+                  break;
+                case firebase.storage.TaskState.RUNNING: // or 'running'
+                  console.log('Upload is running');
+                  break;
+              }
+            }, function(error) {
 
-    // A full list of error codes is available at
-    // https://firebase.google.com/docs/storage/web/handle-errors
+            // A full list of error codes is available at
+            // https://firebase.google.com/docs/storage/web/handle-errors
 
-    switch (error.code) {
-      case 'storage/unauthorized':
-        // User doesn't have permission to access the object
-        break;
+            switch (error.code) {
+              case 'storage/unauthorized':
+                // User doesn't have permission to access the object
+                break;
 
-      case 'storage/canceled':
-        // User canceled the upload
-        break;
+              case 'storage/canceled':
+                // User canceled the upload
+                break;
 
-      case 'storage/unknown':
-        // Unknown error occurred, inspect error.serverResponse
-        break;
-    }
+              case 'storage/unknown':
+                // Unknown error occurred, inspect error.serverResponse
+                break;
+            }
 
-  }, function() {
-    // Upload completed successfully, now we can get the download URL
-    var downloadURL = uploadTask.snapshot.downloadURL;
+          }, function() {
+            // Upload completed successfully, now we can get the download URL
+            var downloadURL = uploadTask.snapshot.downloadURL;
+          });
   });
-    });
-
-  }
+}
 
 /////////// This part is for downloading from firebase:
-function downloadFromFirebase() {
+function downloadFromFirebase(name) {
   // Create a reference to the file we want to download
   var storageRef = firebase.storage().ref();
-  var imgRef = storageRef.child('canvas_images/canvas.jpeg');
+  var canvasRef = storageRef.child('canvas_images');
+  var fileName = name;
+  var imgRef = canvasRef.child(fileName);
+  //var imgRef = storageRef.child('canvas_images/canvas.jpeg');
 
   // Get the download URL
   imgRef.getDownloadURL().then(function(url) {
