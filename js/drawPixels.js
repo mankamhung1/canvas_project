669afe1273e680750e3358bcd76ad4a6f58d50fd
jason13245 <jason13245@gmail.com>
2017-11-02 10:25:01 +0800

    Added colored pixels feature

---
+++ js/drawPixels.js
@@ -0,0 +1,32 @@
+class DrawingRandomPixels extends PaintFunction{
+    constructor(contextReal){
+        super();
+        this.context = contextReal;
+    }
+
+    onMouseDown(coord,event){
+        isDrawing = true;
+    }
+    onDragging(coord,event){
+        if (!isDrawing) return;
+        for (let i = -10; i < 10; i+= 4) {
+            for (let j = -10; j < 10; j+= 4) {
+              if (Math.random() > 0.5) {
+                this.context.fillStyle = ['red', 'orange', 'yellow', 'green', 
+                'light-blue', 'blue', 'purple'][getRandomInt(0,6)];
+                this.context.fillRect(coord[0]+i, coord[1]+j, 4, 4);
+              }
+            }
+        }
+      }
+    onMouseMove(coord, event){
+
+    }
+    onMouseUp(){
+        isDrawing = false;
+    }
+    onMouseLeave(){}
+    onMouseEnter(){}
+
+    
+}
