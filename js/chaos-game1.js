0da91ca5d2ada1e0594298617439d2be7907b9ee
jason13245 <jason13245@gmail.com>
2017-11-03 14:20:46 +0800

    improved chaos game

--- js/chaos-game1.js
+++ js/chaos-game1.js
@@ -6,88 +6,108 @@ class ChaosGame1 extends PaintFunction{
         this.contextDraft = contextDraft;
         this.contextReal = contextReal;
         this.state = 'Start';
+        this.points =[];
     }
 
     onMouseDown(coord,event){
         if(this.state==='Start'){
-    //    this.contextReal.fillStyle = fill_color;
-      //  this.contextDraft.fillStyle = fill_color;
-        this.contextDraft.strokeStyle =line_color;
-        this.contextReal.strokeStyle =line_color;
-        this.contextDraft.lineCap = 'round';
-        this.contextDraft.lineJoin = "round";
-        this.contextReal.lineCap = 'round';
-        this.contextReal.lineJoin = "round";
-        this.contextDraft.lineWidth =line_width;
-        this.contextReal.lineWidth =line_width;
-        this.origX = coord[0];
-        this.origY = coord[1];
-        console.log(this.origX,this.origY);
-        this.state = 'afterFirstClick';
+            this.contextDraft.globalAlpha = opacity;
+            this.contextReal.globalAlpha =opacity;
+            this.contextReal.fillStyle = fill_color;
+            this.contextDraft.fillStyle = fill_color;
+            this.contextDraft.strokeStyle =border_color;
+            this.contextReal.strokeStyle =border_color;
+            this.contextDraft.lineCap = 'round';
+            this.contextDraft.lineJoin = "round";
+            this.contextReal.lineCap = 'round';
+            this.contextReal.lineJoin = "round";
+            this.contextDraft.lineWidth =borderwidth;
+            this.contextReal.lineWidth =borderwidth;
+            this.points.push({x: coord[0],y: coord[1]});
+            this.origX = coord[0];
+            this.origY = coord[1];
+            this.state = 'afterFirstClick';
 
-        vertices.push({ x: event.clientX, y: event.clientY });
-        this.contextReal.beginPath();
-        this.contextReal.moveTo(this.origX,this.origY);
+            vertices.push({ x: event.clientX, y: event.clientY });            
+            this.contextReal.beginPath();
+            this.contextReal.moveTo(this.origX,this.origY);
         }else if((coord[0]-this.origX)*(coord[0]-this.origX)+(coord[1]-this.origY)*(coord[1]-this.origY)>1001){
-  //          this.contextReal.beginPath();
-  //          this.contextReal.moveTo(this.origX2,this.origY2);
-            this.contextReal.lineTo(coord[0],coord[1]);
-  //          this.contextReal.fill();
-            this.contextReal.stroke();
-            this.origX2=coord[0];
-            this.origY2=coord[1];
+            this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+            this.points.push( {x: coord[0],y: coord[1]});
+            vertices.push({ x: event.clientX, y: event.clientY });                        
+            this.contextDraft.beginPath();
+            this.contextDraft.moveTo(this.points[0].x,this.points[0].y);
+            for(let i=1;i<this.points.length;i++){
+            this.contextDraft.lineTo(this.points[i].x,this.points[i].y);
+            }
+            this.contextDraft.lineTo(coord[0],coord[1]);
+            //this.contextDraft.closePath();
+            this.contextDraft.fill();
+            this.contextDraft.stroke();
             this.state ='intermediate';
-
-            vertices.push({ x: event.clientX, y: event.clientY });
         }else if((coord[0]-this.origX)*(coord[0]-this.origX)+(coord[1]-this.origY)*(coord[1]-this.origY)<1000){
-//            this.contextReal.beginPath();
-//            this.contextReal.moveTo(this.origX2,this.origY2);
+            this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+            this.contextReal.beginPath();
+            this.contextReal.moveTo(this.points[0].x,this.points[0].y);
+            for(let i=1;i<this.points.length;i++){
+            this.contextReal.lineTo(this.points[i].x,this.points[i].y);
+            }
             this.contextReal.lineTo(coord[0],coord[1]);
-  //          this.contextReal.fill();
+            this.contextReal.fill();
             this.contextReal.stroke();
-            this.contextReal.closePath();
+            //this.contextReal.closePath();
+            drawChaosGame(this.origX,this.origY); // this coord can be anywhere within the canvas.            
             this.state = 'finishpolygon';
-            console.log(vertices);
-            drawChaosGame(this.origX,this.origY); // this coord can be anywhere within the canvas.
+            vertices =[ ];
         }
     }
     onDragging(coord,event){
+        if(this.state === 'afterFirstClick'){
+            this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
+            this.contextDraft.beginPath();
+            this.contextDraft.moveTo(this.origX,this.origY);
+            this.contextDraft.lineTo(coord[0],coord[1]);
+            this.contextDraft.fill('nonzero');
+            this.contextDraft.stroke();
+            this.contextDraft.closePath();
+        }
+
     }
 
     onMouseMove(coord,event){
          if(this.state === 'afterFirstRelease'||this.state ==='intermediate'){
-            console.log('step4');
+
             this.contextDraft.clearRect(0,0,canvasDraft.width,canvasDraft.height);
             this.contextDraft.beginPath();
-            this.contextDraft.moveTo(this.origX2,this.origY2);
+            this.contextDraft.moveTo(this.points[0].x,this.points[0].y);
+            for(let i=1;i<this.points.length;i++){
+            this.contextDraft.lineTo(this.points[i].x,this.points[i].y);
+            }
             this.contextDraft.lineTo(coord[0],coord[1]);
-    //        this.contextReal.lineTo(coord[0],coord[1]);
+            //this.contextDraft.closePath();
+            this.contextDraft.fill();
             this.contextDraft.stroke();
-         }
+        }
     }
     onMouseUp(coord,event){
         if(this.state ==='afterFirstClick'){
-            console.log('step3');
-//            this.contextReal.beginPath();
-//            this.contextReal.moveTo(this.origX,this.origY);
             this.contextReal.lineTo(coord[0],coord[1]);
-            this.contextReal.fill();
             this.contextReal.stroke();
             this.origX2=coord[0];
             this.origY2=coord[1];
-            console.log(this.origX2,this.origY2);
-            this.state = 'afterFirstRelease';
+            this.state='afterFirstRelease';
+            this.points.push({x: coord[0],y: coord[1]});
+            vertices.push({ x: event.clientX, y: event.clientY });                        
         }else if(this.state ==='intermediate'){
-            console.log('hi');
         }
         else if(this.state ==='finishpolygon'){
-            console.log('finish polygon');
             this.contextReal.lineCap = 'butt';
             this.contextReal.lineJoin = "butt";
             this.contextDraft.lineCap = 'butt';
             this.contextDraft.lineJoin = "butt";
             this.state ='Start';
             drawImage();
+            this.points.length=0;
         }
     }
     onMouseLeave(){}
